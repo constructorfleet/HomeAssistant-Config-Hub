87e9105480c9126886224559e104b010e46f28c8
Teagan glenn <teagan.m.glenn@gmail.com>
2020-07-01 04:10:55 -0600

    Hue  Sync integration

---
+++ custom_components/hue_sync_box/__init__.py
@@ -0,0 +1,21 @@
+"""Philips Hue Sync Box integration."""
+
+from homeassistant.helpers import config_validation
+import voluptuous
+
+from . import const
+from. import services
+
+
+
+
+PLATFORM_SCHEMA = config_validation.PLATFORM_SCHEMA.extend({
+    voluptuous.Required(const.CONF_IP_ADDRESS): config_validation.string,
+    voluptuous.Required(const.CONF_ACCESS_TOKEN): config_validation.string
+    voluptuous.Optional(const.CONF_NAME): config_validation.string,
+})
+
+
+async def async_setup(hass, config):
+  hass.data[const.DOMAIN] = {}
+  return True
